1b386ab8ac8000e6adcccba8fc30fe5de50e2989
DLOADIN <m.david@alustudent.com>
2025-02-06 16:18:37 +0200

    done

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -6,12 +6,12 @@ const Links = () => {
   return (
     <>
     <div className="grid items-center justify-center ">
-      <div className="flex items-center justify-center mb-2  space-x-4 py-4 bg-white bg-opacity-5 backdrop-brightness-200 rounded-lg shadow-lg">
+      <div className="flex items-center justify-center mb-2  space-x-4 py-4 bg-black bg-opacity-5 backdrop-brightness-200 rounded-lg shadow-lg">
         <a
           href="https://github.com/DLOADIN"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-white hover:text-[#915EFF] transform hover:scale-110 transition-all duration-500 ease-in pr-4 pl-5"
+          className="text-black hover:text-[#915EFF] transform hover:scale-110 transition-all duration-500 ease-in pr-4 pl-5"
         >
           <Github size={35} />
         </a>
@@ -19,7 +19,7 @@ const Links = () => {
           href="https://instagram.com/__m.a.n.z.i"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-white hover:text-[#915EFF] transform hover:scale-110 transition-all duration-500 ease-in"
+          className="text-black hover:text-[#915EFF] transform hover:scale-110 transition-all duration-500 ease-in"
         >
           <Instagram size={35} />
         </a>
@@ -27,13 +27,13 @@ const Links = () => {
           href="https://www.linkedin.com/in/manzi-david-653769299/"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-white hover:text-[#915EFF] transform hover:scale-110 transition-all duration-500 ease-in  pl-4 pr-5"
+          className="text-black hover:text-[#915EFF] transform hover:scale-110 transition-all duration-500 ease-in  pl-4 pr-5"
         >
           <Linkedin size={35} />
         </a>
       </div>
     </div>
-    <div className='flex flex-col md:flex-row justify-between items-center px-10 py-2'>
+    <div className='flex flex-col md:flex-row justify-between items-center px-10 py-2 text-black'>
         <h5 className='pl-7 text-center md:text-left'>
         This site was rendered in <code className="text-[#915EFF]">React JS</code> with the <code className="text-[#915EFF]">three.js</code> library by David
       </h5>
